b1a88645b2ee1d5ec4d8b8425aaf7bab0f59b52a
SEB <se13codeur@gmail.com>
2021-08-23 18:30:01 +0200

    added routes.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import routes from './routes/routes.js'
 import dotenv from 'dotenv'
 
 import mongoose from 'mongoose'
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 888
 const app = express()
 
 app.use(express.json())
+
+app.use(routes)
 
 mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
