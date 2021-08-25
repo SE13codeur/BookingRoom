fc90d371b9ecf3360f87d6bf8cf11571d03228ea
SEB <se13codeur@gmail.com>
2021-08-25 13:36:34 +0200

    modified App.js

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,25 @@
-import Layout from 'antd/lib/layout/layout'
 import React from 'react'
+import Layout from '../components/Navigation'
+import { Route, Switch } from 'react-router-dom'
+
+import HomePage from '../pages/HomePage'
+import RoomsPage from '../pages/RoomsPage'
+import Contact from '../pages/Contact'
+import NotFound from '../pages/NotFound'
+
+
 
 const App = () => {
     return (
-        <Layout>
-            hej
-        </Layout>
+      <Layout>
+        
+        <Switch>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/' element={<RoomsPage />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='*' element={<NotFound />} />
+        </Switch>
+    </Layout>
     )
 }
 
---
+++ client/src/pages/RoomsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomsPage = () => {
+    return (
+        <div>
+            Rooms
+        </div>
+    )
+}
+
+export default RoomsPage
